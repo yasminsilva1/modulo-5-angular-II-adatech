b1b85ed2ef6fae0177d8fa39889172599f6dc950
Yasmin Silva <yasminsilvadeveloper@gmail.com>
2024-11-18 22:04:14 -0300

    Aula 02 - pt1 - Altera o HeaderComponent e utiliza o RouterLink

--- crud/src/app/common/header/header.component.ts
+++ crud/src/app/common/header/header.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [RouterLink, MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
