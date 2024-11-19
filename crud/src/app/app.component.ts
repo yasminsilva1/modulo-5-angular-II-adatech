f50e08a2e54cf1f17f93db7730625382e6c4b9c0
Yasmin Silva <yasminsilvadeveloper@gmail.com>
2024-11-18 22:23:06 -0300

    Aula 02 - pt8 - Cria rotas

--- crud/src/app/app.component.ts
+++ crud/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './common/header/header.component';
-import { RegisterComponent } from './modules/auth/components/register/register.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, RegisterComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- crud/src/app/app.routes.ts
+++ crud/src/app/app.routes.ts
@@ -1,3 +1,48 @@
 import { Routes } from '@angular/router';
+import { NotFoundComponent } from './common/not-found/not-found.component';
+import { LoginComponent } from './modules/auth/components/login/login.component';
+import { RegisterComponent } from './modules/auth/components/register/register.component';
+import { ProductsCreateComponent } from './modules/products/components/products-create/products-create.component';
+import { ProductsListComponent } from './modules/products/components/products-list/products-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'products',
+    pathMatch: 'full',
+  },
+  {
+    path: 'products',
+    children: [
+      {
+        path: '',
+        component: ProductsListComponent,
+      },
+      {
+        path: 'create',
+        component: ProductsCreateComponent,
+      },
+      {
+        path: 'edit',
+        component: ProductsCreateComponent,
+      },
+    ],
+  },
+  {
+    path: 'auth',
+    children: [
+      {
+        path: 'register',
+        component: RegisterComponent,
+      },
+      {
+        path: 'login',
+        component: LoginComponent,
+      },
+    ],
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
+];
