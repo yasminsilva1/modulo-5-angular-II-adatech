0f00a8e23200309d04e4675663e11c1095c4bdfb
Yasmin Silva <yasminsilvadeveloper@gmail.com>
2024-11-22 17:44:56 -0300

    Atualiza a service

--- crud/src/app/modules/products/services/products.service.ts
+++ crud/src/app/modules/products/services/products.service.ts
@@ -1,17 +1,50 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
 import { Product } from '../models/products.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductsService {
-  apiUrl = 'http://localhost:3000/products';
+  private apiUrl = 'http://localhost:3000/products';
+  private productList$ = new BehaviorSubject<Product[]>([]);
+  products$ = this.productList$.asObservable();
 
   constructor(private http: HttpClient) {}
 
+  private addProduct(newProduct: Product): void {
+    this.productList$.next([...this.productList$.value, newProduct]);
+  }
+
+  private removeProduct(id: string): void {
+    this.productList$.next(
+      this.productList$.value.filter((product) => product.id !== id)
+    );
+  }
+
+  loadProductList() {
+    return this.http.get<Product[]>(this.apiUrl).subscribe({
+      next: (products) => {
+        this.productList$.next(products);
+      },
+      error: (err) => {
+        console.error('Erro ao carregar os produtos:', err);
+      },
+    });
+  }
+
   saveProduct(product: Product): Observable<void> {
-    return this.http.post<void>(this.apiUrl, product);
+    return this.http.post<void>(this.apiUrl, product).pipe(
+      tap(() => {
+        this.addProduct(product);
+      })
+    );
+  }
+
+  deleteProduct(id: string): Observable<void> {
+    return this.http
+      .delete<void>(`${this.apiUrl}/${id}`)
+      .pipe(tap(() => this.removeProduct(id)));
   }
 }
