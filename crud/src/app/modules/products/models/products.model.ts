9d8e3b5fb5dc8e72256154aa8962fceabd5617e7
Yasmin Silva <yasminsilvadeveloper@gmail.com>
2024-11-22 17:44:04 -0300

    Remove o tipo 'null' da interface

--- crud/src/app/modules/products/models/products.model.ts
+++ crud/src/app/modules/products/models/products.model.ts
@@ -1,8 +1,8 @@
 export interface Product {
-  id?: string | null;
-  name: string | null;
-  description: string | null;
-  price: number | null;
-  qty: number | null;
-  image: string | null;
+  id?: string;
+  name: string;
+  description: string;
+  price: number;
+  qty: number;
+  image: string;
 }
