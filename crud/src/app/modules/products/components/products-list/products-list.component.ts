096a0f3901bfba5057240db487c216cdc4b24e32
Yasmin Silva <yasminsilvadeveloper@gmail.com>
2024-11-22 17:48:00 -0300

    Renderiza os produtos na lista

--- crud/src/app/modules/products/components/products-list/products-list.component.ts
+++ crud/src/app/modules/products/components/products-list/products-list.component.ts
@@ -1,12 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
+import { Product } from '../../models/products.model';
+import { ProductsService } from '../../services/products.service';
+import { ProductCardComponent } from '../product-card/product-card.component';
 
 @Component({
   selector: 'app-products-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink, ProductCardComponent, MatButtonModule],
   templateUrl: './products-list.component.html',
-  styleUrl: './products-list.component.css'
+  styleUrl: './products-list.component.css',
 })
 export class ProductsListComponent {
+  products$ = this.productsService.products$;
 
+  constructor(private productsService: ProductsService) {}
+
+  ngOnInit(): void {
+    this.productsService.loadProductList();
+  }
+
+  trackById(product: Product): string {
+    return product.id!;
+  }
 }
