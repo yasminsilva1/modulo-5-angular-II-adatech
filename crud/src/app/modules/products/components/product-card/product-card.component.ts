e2795f05df6633d84534d726d549b3c5d07b71f1
Yasmin Silva <yasminsilvadeveloper@gmail.com>
2024-11-22 23:22:59 -0300

    Aula 03 - pt3 - Adiciona o takeUntilDestroyed no método deleteProduct

--- crud/src/app/modules/products/components/product-card/product-card.component.ts
+++ crud/src/app/modules/products/components/product-card/product-card.component.ts
@@ -1,28 +1,36 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, DestroyRef, Input } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { RouterLink } from '@angular/router';
 import { Product } from '../../models/products.model';
 import { ProductsService } from '../../services/products.service';
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, MatCardModule],
+  imports: [CommonModule, MatButtonModule, MatCardModule, RouterLink],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.css',
 })
 export class ProductCardComponent {
   @Input() product!: Product;
 
-  constructor(private productsService: ProductsService) {}
+  constructor(
+    private productsService: ProductsService,
+    private destroyRef: DestroyRef
+  ) {}
 
   deleteProduct(id: string) {
-    this.productsService.deleteProduct(id).subscribe({
-      next: () => {},
-      error: (err) => {
-        console.error('Erro ao deletar produto: ', err);
-      },
-    });
+    this.productsService
+      .deleteProduct(id)
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe({
+        complete: () => {},
+        error: (err) => {
+          console.error('Erro ao deletar produto: ', err);
+        },
+      });
   }
 }
