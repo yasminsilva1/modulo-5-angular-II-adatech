fec7c0a50052f2de3a413bbfc10b525d5283a969
Yasmin Silva <yasminsilvadeveloper@gmail.com>
2024-11-25 20:51:28 -0300

    Renomeia a service Register para Auth

---
+++ crud/src/app/modules/auth/services/auth.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../models/user.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private apiUrl = 'http://localhost:3000/auth';
+
+  constructor(private http: HttpClient) {}
+
+  register(user: User): Observable<void> {
+    return this.http.post<void>(`${this.apiUrl}/register`, user);
+  }
+}
