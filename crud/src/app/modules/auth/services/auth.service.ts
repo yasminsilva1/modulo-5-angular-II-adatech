9e4edbf774279ed0fbfda9e87f6bc6c274ae0dc0
Yasmin Silva <yasminsilvadeveloper@gmail.com>
2024-11-25 22:54:14 -0300

    Aula 04 - pt8 - Adiciona o método de login

--- crud/src/app/modules/auth/services/auth.service.ts
+++ crud/src/app/modules/auth/services/auth.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { UserCredentials } from '../models/user-credentials.model';
 import { User } from '../models/user.model';
+import { AuthenticatedUser } from './../models/authenticated-user.model';
 
 @Injectable({
   providedIn: 'root',
@@ -13,5 +15,12 @@ export class AuthService {
 
   register(user: User): Observable<void> {
     return this.http.post<void>(`${this.apiUrl}/register`, user);
+  }
+
+  login(credential: UserCredentials): Observable<AuthenticatedUser> {
+    return this.http.post<AuthenticatedUser>(
+      `${this.apiUrl}/login`,
+      credential
+    );
   }
 }
