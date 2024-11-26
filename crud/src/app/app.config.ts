6a19f25275ea7a716d4acf5e4ba94884bb1f3b4e
Yasmin Silva <yasminsilvadeveloper@gmail.com>
2024-11-25 21:00:16 -0300

    Aula 04 - pt1 - Instala o NgxMask

--- crud/src/app/app.config.ts
+++ crud/src/app/app.config.ts
@@ -4,11 +4,13 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
+import { provideEnvironmentNgxMask } from 'ngx-mask';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimationsAsync(),
     provideHttpClient(),
+    provideEnvironmentNgxMask(),
   ],
 };
