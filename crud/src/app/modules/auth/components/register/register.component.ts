80025cd777c0fad9adb48ed711d56b9b8cfa343f
Yasmin Silva <yasminsilvadeveloper@gmail.com>
2024-11-25 22:53:02 -0300

    Aula 04 - pt7 - Cria um validador de CPF

--- crud/src/app/modules/auth/components/register/register.component.ts
+++ crud/src/app/modules/auth/components/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, DestroyRef } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import {
   FormControl,
+  FormControlState,
   FormGroup,
   ReactiveFormsModule,
   Validators,
@@ -9,10 +10,10 @@ import {
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
 import { Router } from '@angular/router';
+import { NgxMaskDirective } from 'ngx-mask';
 import { SnackBarService } from '../../../../utils/snack-bar.service';
-import { RegisterService } from '../../services/register.service';
+import { AuthService } from '../../services/auth.service';
 import { ZipCodeService } from '../../services/zipCode.service';
 
 @Component({
@@ -22,8 +23,8 @@ import { ZipCodeService } from '../../services/zipCode.service';
     MatInputModule,
     MatFormFieldModule,
     MatButtonModule,
-    MatSelectModule,
     ReactiveFormsModule,
+    NgxMaskDirective,
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
@@ -33,17 +34,22 @@ export class RegisterComponent {
     name: new FormControl(null, [Validators.required]),
     profession: new FormControl(null, [Validators.required]),
     birthDate: new FormControl(null, [Validators.required]),
-    documentNumber: new FormControl(null, [Validators.required]),
+    documentNumber: new FormControl(null, [
+      Validators.minLength(11),
+      Validators.maxLength(11),
+      this.validateDocumentNumber,
+    ]),
     phone: new FormControl(null, [Validators.required]),
     email: new FormControl(null, [Validators.required]),
     password: new FormControl(null, [Validators.required]),
+    passwordConfirm: new FormControl(null, [Validators.required]),
     address: new FormGroup({
       zipCode: new FormControl(null, [Validators.required]),
       street: new FormControl({ value: null, disabled: true }, [
         Validators.required,
       ]),
       number: new FormControl(null, [Validators.required]),
-      complement: new FormControl(null),
+      complement: new FormControl(null, [Validators.required]),
       neighborhood: new FormControl({ value: null, disabled: true }, [
         Validators.required,
       ]),
@@ -58,20 +64,20 @@ export class RegisterComponent {
 
   constructor(
     private zipCodeService: ZipCodeService,
-    private registerService: RegisterService,
+    private authService: AuthService,
     private destroyRef: DestroyRef,
     private router: Router,
     private snackBarService: SnackBarService
   ) {}
 
   onSubmit() {
-    this.registerService
-      .addUser(this.form.getRawValue())
+    this.authService
+      .register(this.form.getRawValue())
       .pipe(takeUntilDestroyed(this.destroyRef))
       .subscribe({
         complete: () => {
           this.snackBarService.openSnackBar('Usuário cadastrado com sucesso!');
-          this.router.navigate(['/products']);
+          this.router.navigate(['/auth/login']);
         },
         error: (err) => {
           this.snackBarService.openSnackBar('Erro ao cadastrar usuário.');
@@ -115,6 +121,47 @@ export class RegisterComponent {
         },
       });
     }
+  }
+
+  // Método que valida se um CPF fornecido é válido
+  private validateDocumentNumber({
+    value,
+  }: FormControlState<string>): { [key: string]: boolean } | null {
+    if (!value) return { requiredDocument: true };
+
+    value = value.replace(/[^\d]+/g, '');
+    if (value == '') return { emptyDocument: true };
+
+    // Elimina CPFs invalidos conhecidos
+    if (value.length != 11 || value === value[0].repeat(value.length)) {
+      return { repeatCharacters: true };
+    }
+
+    // Valida 1o digito
+    let add = 0;
+
+    for (let i = 0; i < 9; i++) {
+      add += parseInt(value.charAt(i)) * (10 - i);
+    }
+
+    let rev = 11 - (add % 11);
+
+    if (rev == 10 || rev == 11) rev = 0;
+
+    if (rev != parseInt(value.charAt(9))) return { invalidDocument: true };
+
+    // Valida 2o digito
+    add = 0;
+    for (let i = 0; i < 10; i++) {
+      add += parseInt(value.charAt(i)) * (11 - i);
+    }
+
+    rev = 11 - (add % 11);
+
+    if (rev == 10 || rev == 11) rev = 0;
+    if (rev != parseInt(value.charAt(10))) return { invalidDocument: true };
+
+    return null;
   }
 
   cancelForm() {
