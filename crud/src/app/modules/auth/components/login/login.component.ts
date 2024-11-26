8747cdfdab8fc9fb7e4007d437859cfe1eeca04d
Yasmin Silva <yasminsilvadeveloper@gmail.com>
2024-11-25 22:51:56 -0300

    Aula 04 - pt6 - Cria o template do formulário de login

--- crud/src/app/modules/auth/components/login/login.component.ts
+++ crud/src/app/modules/auth/components/login/login.component.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  form: FormGroup = new FormGroup({
+    email: new FormControl(null, [Validators.required]),
+    password: new FormControl(null, [Validators.required]),
+  });
 
+  constructor(
+    private authService: AuthService,
+    private destroyRef: DestroyRef,
+    private router: Router
+  ) {}
+
+  onSubmit() {
+    this.authService
+      .login(this.form.getRawValue())
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe({
+        next: (res) => {
+          sessionStorage.setItem('USER_TOKEN', res.token);
+          this.router.navigate(['/products']);
+        },
+        error: (err) => {
+          console.error(err);
+        },
+      });
+  }
 }
